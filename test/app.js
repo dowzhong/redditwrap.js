4be6353f115b7ff152c2288c6e06a90effddcc39
Martijn Vegter <mvegter.coding@gmail.com>
2017-08-19 15:06:02 +0200

    Add Travis Test

---
+++ test/app.js
@@ -0,0 +1,15 @@
+const redditjs = require('../lib/main.js') // use require('redditjs') in production
+let reddit = new redditjs({
+  username: process.env.username,
+  password: process.env.password,
+  clientID: process.env.clientID,
+  clientSecret: process.env.clientSecret
+})
+
+reddit.submitTextPost('test', 'Hello World!', 'My first text post!')
+  .then(data =>{
+    console.log(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
